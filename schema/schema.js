5eae7f0b344fca8d44d17913be6b652b8173ebc0
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-10-17 12:19:08 +0300

    company to id, graphqllist

--- schema/schema.js
+++ schema/schema.js
@@ -6,7 +6,8 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -14,7 +15,14 @@ const CompanyType = new GraphQLObjectType({
     fields: {
         id: {type: GraphQLString},
         name: {type: GraphQLString},
-        description: {type: GraphQLString}
+        description: {type: GraphQLString},
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    then(res => res.data)
+            }
+        }
     }
 });
 
@@ -42,7 +50,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: {type: GraphQLString} },
             resolve(parentValue, args) {
                 return axios.get(`http://localhost:3000/users/${args.id}`)
-                .then(res => res.data)
+                    .then(res => res.data)
             } 
         },
         company: {
